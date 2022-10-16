c70e90512ba875a2904b09ad61b943fdb095a312
Adryan Eka Pramudita <adryan160102@gmail.com>
2022-10-16 18:45:35 +0700

    first commit

---
+++ looping.php
@@ -0,0 +1,10 @@
+<?php
+for ($i = 0; $i < 10; $i++) {
+    echo "looping ke $i<br>";
+}
+
+//gunakan  .  untuk menggabungkan
+$animals = ['cat', 'dog', 'fish'];
+foreach ($animals as $animal) {
+    echo $animal . '<br>';
+}
---
+++ variable.php
@@ -0,0 +1,15 @@
+<?php
+
+//membuat variable
+//integer,float,double,boolean tidak perlu "..."
+$nama = "Adryan Eka Pramudita";
+$jurusan = "Teknik Informatika";
+$umur = 20;
+
+//memanggil variable
+// br untuk menggunakan enter
+echo $nama;
+echo "<br>";
+echo $jurusan;
+echo "<br>";
+echo $umur;
